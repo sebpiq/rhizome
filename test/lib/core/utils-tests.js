c7019bf18468f47fcefa04b70bd45f07587fc939
Sébastien Piquemal <sebpiq@gmail.com>
2015-08-20 10:59:39 +0300

    getFreeFilePath function replaced by node-tmp. fixes #103

--- test/lib/core/utils-tests.js
+++ test/lib/core/utils-tests.js
@@ -7,60 +7,6 @@ var fs = require('fs')
 
 describe('core.utils', function() {
 
-  describe('getFreeFilePath', function() {
-    
-    var symLinked = '/tmp/1234'
-    after(function(done) {
-      var series = _.range(Math.pow(2, 8)).map(function(ind) {
-        var filePath = '/tmp/' + ind.toString(10)
-        return function(next) { fs.unlink(filePath, next) }
-      })
-      series.push(function(next) { fs.unlink(symLinked, next) })
-      async.series(series, done)
-    })
-
-    it('should pick a filepath that doesn\'t exist and create an empty file', function(done) {
-      utils.getFreeFilePath('/tmp', function(err, path) {
-        if (err) throw err
-        assert.ok(path.length > 4)
-        done()
-      })
-    })
-
-    it('should create the file with the given extension', function(done) {
-      utils.getFreeFilePath('/tmp', function(err, path) {
-        if (err) throw err
-        assert.ok(path.length > 4)
-        assert.equal(path.slice(-4), '.wav')
-        done()
-      }, '.wav')
-    })
-
-    it('should fail if there is not file path available', function(done) {
-      // We make so that there is no file path available.
-      // For this we create all the filepaths possibles (except one : /tmp/255), symlinks and real files.
-      var series = _.range(Math.pow(2, 8) - 1).map(function(ind) {
-        var filePath = '/tmp/' + ind.toString(10)
-        return function(next) {
-          if (Math.random() < 0.5) fs.symlink(symLinked, filePath, next)
-          else fs.writeFile(filePath, '', next)
-        }
-      })
-      // We also create the file we symlink
-      series.unshift(function(next) { fs.writeFile(symLinked, '', next) })
-      // The try to get a free filename ... which should fail
-      series.push(function(next) {
-        utils.getFreeFilePath('/tmp', function(err, path) {
-          if (err) throw err
-          assert.equal(path, '/tmp/255')
-          next()
-        }, '', 1)
-      })
-      async.series(series, done)
-    })
-
-  })
-
   describe('saveBlob', function() {
 
     it('should save the blob in the given directory and pick a name automatically', function(done) {
