6eb8e8625f85a3ed18ea3e36d1c30201dc9e5d74
Sébastien Piquemal <sebpiq@gmail.com>
2016-01-12 17:30:49 +0200

    fixed style and organization for metronome example

--- examples/metronome/config.js
+++ examples/metronome/config.js
@@ -1,32 +1,32 @@
-var fs = require('fs');
-var path = require('path');
-var storeDir= path.join(__dirname, 'tmp');
-if (!fs.existsSync(storeDir)) fs.mkdirSync(storeDir);
+var fs = require('fs')
+  , path = require('path')
+  , storeDir= path.join(__dirname, 'tmp')
+if (!fs.existsSync(storeDir)) fs.mkdirSync(storeDir)
 
-module.exports= {
-    servers: [
-        {
-            type: 'http',
-            config: {
-                port: 8000,
-                staticDir: path.join(__dirname, 'app')
-            }
-        },
-        {
-            type: 'osc',
-            config: {
-                port: 9000
-            }
-        },
-        {
-            type: 'websockets',
-            config: {
-                port: 8000,
-                maxSockets: 5
-            }
-        }
-    ],
-    connections: {
-        store: storeDir
+module.exports = {
+  servers: [
+    {
+      type: 'http',
+      config: {
+        port: 8000,
+        staticDir: path.join(__dirname, 'pages')
+      }
+    },
+    {
+      type: 'osc',
+      config: {
+        port: 9000
+      }
+    },
+    {
+      type: 'websockets',
+      config: {
+        port: 8000,
+        maxSockets: 5
+      }
     }
+  ],
+  connections: {
+    store: storeDir
+  }
 }
